cbe17f0980edba71e08f5bd4016ed8257ee13e92
Darrell Schiebel <darrell@schiebel.us>
2021-06-30 13:44:32 -0400

    attempt (#2) to get the python docs to build on readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,7 @@ from links import *
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.napoleon','sphinx.ext.todo','xref']
+extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon','sphinx.ext.todo','xref']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
