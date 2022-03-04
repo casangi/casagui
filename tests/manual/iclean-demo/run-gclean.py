058da9f47dff9a8f7944da4a87ad3403bb9ce648
Darrell Schiebel <darrell@schiebel.us>
2022-03-04 11:48:26 -0500

    add code to fetch the test MS if it doesn't exist

--- tests/manual/iclean-demo/run-gclean.py
+++ tests/manual/iclean-demo/run-gclean.py
@@ -1,4 +1,22 @@
+import os
+import urllib
+import tarfile
+
 from casatasks.private.imagerhelpers._gclean import gclean
+
+##
+## demo measurement set to use
+##
+ms_path = 'refim_point_withline.ms'
+##
+## where to fetch the demo measurement set
+##
+ms_url = "https://casa.nrao.edu/download/devel/casavis/data/refim_point_withline-ms.tar.gz"
+
+if not os.path.isdir(ms_path):
+    tstream = urllib.request.urlopen(ms_url)
+    tar = tarfile.open(fileobj=tstream, mode="r:gz")
+    tar.extractall()
 
 def main( ):
     for rec in gclean( vis='refim_point_withline.ms', imagename='test', niter=20 ):
