8db0df9c96e51551510fc734a5b4e219381f273f
Darrell Schiebel <darrell@schiebel.us>
2022-08-01 16:58:10 -0400

    decouple casagui version from casaguijs version

--- casagui/bokeh/state/_initialize.py
+++ casagui/bokeh/state/_initialize.py
@@ -31,10 +31,10 @@ applications produce'''
 from os.path import dirname, join, basename
 from bokeh import resources
 from ...utils import path_to_url, static_vars, have_network
-from casagui import __version__ as VERSION
 from bokeh import __version__ as bokeh_version
 
 CASALIB_VERSION = "0.0.1"
+CASAGUIJS_VERSION = "0.0.6"
 
 @static_vars(initialized=False,do_local_subst=not have_network( ))
 def initialize_bokeh( js=None, bokeh=None, bokeh_dev=0 ):
@@ -113,14 +113,14 @@ def initialize_bokeh( js=None, bokeh=None, bokeh_dev=0 ):
     ### Initialize the Bokeh dependent library(ies)
     ###
     if bokeh is None:
-        bokehlib = f"casaguijs-v{VERSION}.{bokeh_dev}-b{bokeh_major_minor}.min.js"
+        bokehlib = f"casaguijs-v{CASAGUIJS_VERSION}.{bokeh_dev}-b{bokeh_major_minor}.min.js"
         if initialize_bokeh.do_local_subst:
             bokehlib_url = path_to_url( join( dirname(__file__), 'js', bokehlib ) )
         else:
             ### ------------------------------------------------------------------------------------------
             ### should potentially find a better download location...
             ### ------------------------------------------------------------------------------------------
-            bokehlib_url = f"https://casa.nrao.edu/download/javascript/casaguijs/{VERSION}/{bokehlib}"
+            bokehlib_url = f"https://casa.nrao.edu/download/javascript/casaguijs/{CASAGUIJS_VERSION}/{bokehlib}"
         bokehlib_libs = [ bokehlib_url ]
     else:
         bokehlib_libs = [ bokeh ] if isinstance(bokeh,str) else bokeh
@@ -141,7 +141,7 @@ def initialize_bokeh( js=None, bokeh=None, bokeh_dev=0 ):
             ### ------------------------------------------------------------------------------------------
             ### should potentially find a better download location...
             ### ------------------------------------------------------------------------------------------
-            jslib_url = f"https://casa.nrao.edu/download/javascript/casaguijs/{VERSION}/{jslib}"
+            jslib_url = f"https://casa.nrao.edu/download/javascript/casaguijs/{CASAGUIJS_VERSION}/{jslib}"
         jslib_libs = [ jslib_url ]
     else:
         jslib_libs = [ js ] if isinstance(js,str) else js
