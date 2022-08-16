a1bb39326b961c94b680845ca7b758469baf0a40
Darrell Schiebel <darrell@schiebel.us>
2022-08-16 12:20:33 -0400

    add hotkeys-js to casalib

--- casalib/src/browser.ts
+++ casalib/src/browser.ts
@@ -4,5 +4,8 @@
  */
 import { object_id } from './object_id'
 
+import hotkeys from 'hotkeys-js'
+export { hotkeys }
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 (window as any).object_id = object_id
