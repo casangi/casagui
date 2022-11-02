73b988311884ec575cfabdab1ac3500d018b6ed8
Darrell Schiebel <darrell@schiebel.us>
2022-11-01 22:01:05 -0400

    add enum class that supports documentation [CAS-13743]

---
+++ casagui/utils/_docenum.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class DocEnum(Enum):
+    def __new__(cls, value, doc=None):
+        self = object.__new__(cls)  # calling super().__new__(value) here would fail
+        self._value_ = value
+        if doc is not None:
+            self.__doc__ = doc
+        return self
