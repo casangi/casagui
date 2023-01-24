a101bb886f0fc60bd0cfb0c80f816ae23dd7c2d2
Darrell Schiebel <darrell@schiebel.us>
2023-01-24 14:02:57 -0500

    websockets server should expect dropped connections from browser (closed laptop, etc)

--- tests/manual/websocket-reconnect/server.py
+++ tests/manual/websocket-reconnect/server.py
@@ -1,14 +1,20 @@
 import time
 import asyncio
 import websockets
+import traceback
 
 message_count = 0
 async def timeserver( websocket ):
     global message_count
-    async for message in websocket:
-        message_count += 1
-        print(f'{message_count}\t{message}')
-        await websocket.send(time.strftime('%Y-%m-%d.%H:%M:%S'))
+    try:
+        async for message in websocket:
+            message_count += 1
+            print(f'{message_count}\t{message}')
+            await websocket.send(time.strftime('%Y-%m-%d.%H:%M:%S'))
+    except websockets.exceptions.ConnectionClosedError:
+        print('browser connection dropped...')
+    except Exception:
+        print(traceback.print_exc())
 
 async def main( ):
     async with websockets.serve( timeserver, "localhost", 8765 ):
