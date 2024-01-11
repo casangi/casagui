b24130c8302de47e50cd86a1845f2cd00eb03e3e
Darrell Schiebel <darrell@schiebel.us>
2024-01-11 17:47:40 -0500

    add link for jupyter protocol

---
+++ docs/source/links/jupyter_protocol.py
@@ -0,0 +1,6 @@
+from . import link
+
+link_name = "jupyterprotocol"
+user_text = "Jupyter Protocol"
+url = "https://jupyter-protocol.readthedocs.io/en/latest/"
+link.xref_links.update({link_name: (user_text, url)})
