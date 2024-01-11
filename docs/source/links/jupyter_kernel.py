9218da58adffbdac86cb19ca7b9632fdd21223f3
Darrell Schiebel <darrell@schiebel.us>
2024-01-11 17:39:48 -0500

    add link for jupyter kernel

---
+++ docs/source/links/jupyter_kernel.py
@@ -0,0 +1,6 @@
+from . import link
+
+link_name = "jupyterkernel"
+user_text = "Jupyter Kernel"
+url = "https://ipykernel.readthedocs.io/en/stable/"
+link.xref_links.update({link_name: (user_text, url)})
