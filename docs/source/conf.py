503abcb7509ca56e7d5cdc1b904ea1b4a752b888
Darrell Schiebel <darrell@schiebel.us>
2024-01-22 17:40:10 -0500

    update copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, abspath(join(dirname(__file__),'..','..')))         # For aut
 # -- Project information -----------------------------------------------------
 
 project = 'casagui'
-copyright = '2021,2022, Associated Universities, Inc. Washington DC, USA.'
+copyright = '2021,2022,2023,2024 Associated Universities, Inc. Washington DC, USA.'
 author = 'CASA visualization team'
 
 # The full version, including alpha/beta/rc tags
