7fab0005049c17e17107dac451ff8e83fcfb4181
Darrell Schiebel <darrell@schiebel.us>
2024-01-22 17:28:53 -0500

    begin to separate applications+api from design+implementation

---
+++ docs/source/links/casa7.py
@@ -0,0 +1,6 @@
+from . import link
+
+link_name = "casa7"
+user_text = "CASA 7"
+url = "https://safe.nrao.edu/wiki/pub/Software/CASA/CASAUsersCommittee2022/Visualization%20-%20CUC%20Nov%202022.pdf"
+link.xref_links.update({link_name: (user_text, url)})
