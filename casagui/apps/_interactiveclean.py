ad4e14422a56df61d46df69dc2e11f75521d00db
Darrell Schiebel <darrell@schiebel.us>
2024-02-01 20:23:50 -0500

    Revert "change tab to use vis MS base name"
    This reverts commit bf40c0d7cacf2d670a8c40b05b7f36871d30f8b9.

--- casagui/apps/_interactiveclean.py
+++ casagui/apps/_interactiveclean.py
@@ -33,7 +33,6 @@ import asyncio
 import shutil
 import websockets
 from uuid import uuid4
-from pathlib import Path
 from html import escape as html_escape
 from contextlib import asynccontextmanager
 from bokeh.models import Button, TextInput, Div, LinearAxis, CustomJS, Spacer, Span, HoverTool, DataRange1d, Step, InlineStyleSheet
@@ -219,7 +218,7 @@ class InteractiveClean:
         ### Create application context (which includes a temporary directory).
         ### This sets the title of the plot.
         ###
-        self._app_state = AppContext( Path(vis).stem )
+        self._app_state = AppContext( 'Interactive Clean' )
 
         ###
         ### Whether or not the Interactive Clean session is running remotely
