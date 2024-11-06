10891b5edd4d4045fe54e47888f4bd00035063a6
Darrell Schiebel <darrell@schiebel.us>
2024-11-06 12:28:15 -0500

    dependabot cannot stay out of the attic (see branch attic-06NOV2024)

--- attic/bokeh-2/electron-prototype/docs/source/links/casa.py
+++
@@ -1,6 +0,0 @@
-from . import link
-
-link_name = "casa"
-user_text = "CASA"
-url = "https://casa.nrao.edu/"
-link.xref_links.update({link_name: (user_text, url)})
--- attic/bokeh-2/electron-prototype/prototype/iclean/components/button/__init__.py
+++
@@ -1,5 +0,0 @@
-__version__ = '1.0.0'
-
-from .iclean_button import ICleanButton
-
-__all__ = ("ICleanButton")
