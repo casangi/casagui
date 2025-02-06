a6efd3c9186c37301763974b101c4bddfecc3309
Pam Harris <pharris@nrao.edu>
2025-02-06 11:41:49 -0700

    Sort xds by time before concat, scan changed from number to name

--- casagui/data/measurement_set/_ms_concat.py
+++ casagui/data/measurement_set/_ms_concat.py
@@ -16,26 +16,30 @@ def concat_ps_xds(ps, logger):
         logger.debug("Processing set contains one dataset, nothing to concat.")
         return ps.get(0)
 
-    # Split xds by scan numbers
+    # Split xds by time gaps
+    sorted_times = _get_sorted_times(ps)
     xds_list = []
-    scan_list = []
+    time_list = []
     for xds in ps.values():
-        scans, xdss = _split_xds_by_scan(xds)
-        scan_list.extend(scans)
+        xdss, times = _split_xds_by_time_gap(xds, sorted_times)
         xds_list.extend(xdss)
+        time_list.extend(times)
 
-    # Create sorted xds list using sorted scan numbers
-    scan_list.sort()
-    sorted_xds = [None] * len(scan_list)
+    if len(xds_list) > ps_len:
+        logger.debug(f"Split {ps_len} datasets by time gap into {len(xds_list)} datasets.")
+
+    # Create sorted xds list using sorted times
+    time_list.sort()
+    sorted_xds = [None] * len(time_list)
 
     for xds in xds_list:
         try:
-            first_scan = xds.scan_number.values[0]
+            first_xds_time = xds.time.values[0]
         except IndexError: # only one value
-            first_scan = xds.scan_number.values
+            first_xds_time = xds.time.values
 
-        for idx, value in enumerate(scan_list):
-            if value == first_scan and sorted_xds[idx] is None:
+        for idx, value in enumerate(time_list):
+            if value == first_xds_time and sorted_xds[idx] is None:
                 if "baseline" in xds.coords:
                     # Cannot concat with non-dim string coord
                     xds = xds.drop("baseline_antenna1_name")
@@ -46,12 +50,44 @@ def concat_ps_xds(ps, logger):
                 break
     return xr.concat(sorted_xds, dim='time')
 
-def _split_xds_by_scan(xds):
-    ''' Split xds according to scan number.
-        Return list of xds and scan number for each. '''
-    scan_list = []
+def _get_sorted_times(ps):
+    values = []
+    for key in ps:
+        time_values = ps[key].time.values
+        if time_values.size > 1:
+            values.extend(time_values.tolist())
+        else:
+            values.append(time_values)
+    return sorted(values)
+
+def _split_xds_by_time_gap(xds, sorted_times):
+    ''' Split xds where there is a gap in sorted times.
+        Return list of xds and first time in each one. '''
+    times = xds.time.values.ravel()
     xds_list = []
-    for scan in sorted(set(xds.scan_number.values)):
-        scan_list.append(scan)
-        xds_list.append(xds.sel(time=xds.scan_number==scan))
-    return scan_list, xds_list
+    first_times = [times[0]]
+
+    if len(times) == 1:
+        xds_list.append(xds)
+    else:
+        sorted_time_idx = sorted_times.index(times[0])
+        xds_start_idx = 0 # start for iselection
+
+        for idx, time in enumerate(times):
+            if time == sorted_times[sorted_time_idx]:
+                # No time gap, go to next time
+                sorted_time_idx += 1
+                continue
+
+            # Found time gap, select xds
+            xds_list.append(xds.isel(time=slice(xds_start_idx, idx)))
+
+            # start next xds with new first time, find idx in sorted times (skip ahead)
+            xds_start_idx = idx
+            first_times.append(times[idx])
+            sorted_time_idx = sorted_times.index(time) + 1 # for next time
+
+        # add last time range xds
+        xds_list.append(xds.isel(time=slice(xds_start_idx, idx + 1)))
+
+    return xds_list, first_times
